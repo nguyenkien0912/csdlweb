132c1a3d7dd77d3c0ff46067433f8b0c7059cabb
Nguyễn Trung Kiên <trungkien091205@gmail.com>
2025-12-11 20:00:01 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 print("Welcome to Python programming./")
 print("Chao mung den voi lap trinh Python.")
 print("Hello, World!")
-print("Xin chao, The gioi!")
-=======
-print("Welcome to Python programming.///")
->>>>>>> 7be6d0f50a7b73d92f014ef4f5428399a7cee4b8
+
